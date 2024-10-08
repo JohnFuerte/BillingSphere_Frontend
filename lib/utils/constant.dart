1d83d25b3454c96563d24af9b8d04bbebc6191c1
anands2315 <anandsinghfuerte@gmail.com>
2024-10-08 18:38:05 +0530

    as

--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -5,9 +5,10 @@ class Constants {
   // static String baseUrl =
   //     'https://billingsphere-backend-yogeshbhai-2.onrender.com/api';
 
-  // static String baseUrl = 'https://billing-sphere-backend.onrender.com/api';
+  static String baseUrl = 'https://billing-sphere-backend.onrender.com/api';
 
-  static String baseUrl = 'http://192.168.0.117:4567/api';
+  // static String baseUrl = 'http://192.168.0.117:4567/api';
+  // static String baseUrl = 'https://65.1.89.63/api';
   // static String baseUrl = 'https://65.1.89.63/api';
 
   static Widget loadingIndicator = Lottie.asset(
